106a05c947f899b1d3feec855070ee870a93fc80
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-01-12 17:41:51 -0800

    add option column

--- db/migrate/20180113014102_add_option_to_product.rb
+++ db/migrate/20180113014102_add_option_to_product.rb
@@ -1,4 +1,5 @@
 class AddOptionToProduct < ActiveRecord::Migration[5.1]
   def change
+    add_column :products, :option, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180113013600) do
+ActiveRecord::Schema.define(version: 20180113014102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20180113013600) do
     t.decimal "pricing", precision: 10, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "option"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
