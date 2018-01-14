22b9810ace68347a4bbcd5ba8c436e74042740ae
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-01-13 19:07:37 -0800

    add all controllers

---
+++ app/controllers/api/options_controller.rb
@@ -0,0 +1,2 @@
+class Api::OptionsController < ApplicationController
+end
---
+++ test/controllers/api/subcategories_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Api::SubcategoriesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
