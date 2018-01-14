125f6ae4678fbbf295110c5538d1a6529cb86483
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-01-13 19:01:17 -0800

    add bin file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'categories/index'
+  end
+
+  namespace :api do
+    get 'categories/new'
+  end
+
+  namespace :api do
+    get 'categories/edit'
+  end
+
+  namespace :api do
+    get 'categories/create'
+  end
+
+  namespace :api do
+    get 'categories/update'
+  end
+
+  namespace :api do
+    get 'categories/destroy'
+  end
+
+  namespace :api do
+    get 'categories/show'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
