e76f710a3a67d29d7acf89e45abdd14a1039ef28
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-01-13 19:09:02 -0800

    config/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  
+
+  namespace :api, defaults: {format: :json} do
+    resources :categories do
+      resources :subcategories
+    end
+    resources :products
+    resources :images
+    resources :options
+    resources :information
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
