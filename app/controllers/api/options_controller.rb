c920719dbc9ddb00ec7b885df067f7c1da7cbf0a
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-01-16 13:58:59 -0800

    add option controller

--- app/controllers/api/options_controller.rb
+++ app/controllers/api/options_controller.rb
@@ -1,2 +1,16 @@
 class Api::OptionsController < ApplicationController
+
+  def create
+    @option = Option.new(option_params)
+    if @option.save!
+      @product = Product.find_by(id: params[:product_id])
+      render json: "api/products/show", product: @product
+    else
+      render json: @option.errors.full_messages
+    end
+  end
+
+  def option_params
+    params.permit(:product_id, :option_id)
+  end
 end
