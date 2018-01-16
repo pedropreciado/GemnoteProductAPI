77e3ea33543549f299199bdec27c63a9e3ccf25d
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-01-16 13:59:41 -0800

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :subcategories, only: [:create]
     resources :information, only: [:create]
     resources :images, only: [:create]
-
+    resources :options, only: [:create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
