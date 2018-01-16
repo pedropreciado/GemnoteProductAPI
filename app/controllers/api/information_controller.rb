337ad9f426a562b44e2877ca0176f8a6b89a90cc
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-01-16 13:41:00 -0800

    fix render error

--- app/controllers/api/information_controller.rb
+++ app/controllers/api/information_controller.rb
@@ -2,7 +2,7 @@ class Api::InformationController < ApplicationController
   def create
     @information = Information.new(info_params)
     if @information.save!
-      @product = Product.find_by(params[:product_id])
+      @product = Product.find_by(id: params[:product_id])
       render "api/products/show", product: @product
     else
       render json: @information.errors.full_messages
