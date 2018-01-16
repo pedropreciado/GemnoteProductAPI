c4b9cafa8e6aec2ce1386be8cf35b413e4944ec1
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-01-16 13:05:42 -0800

    subcategory controller

--- app/controllers/api/subcategories_controller.rb
+++ app/controllers/api/subcategories_controller.rb
@@ -1,2 +1,16 @@
 class Api::SubcategoriesController < ApplicationController
+
+  def create
+    @subcategory = Subcategory.new(subcategory_params)
+    if @subcategory.save
+      @category = Category.find_by(id: params[:parent_category_id])
+      render "api/cateories/show", category: @category
+    else
+      render json @subcategory.errors.full_messages
+    end
+  end
+
+  def subcategory_params
+    params.permit(:parent_category_id, :child_category_id)
+  end
 end
